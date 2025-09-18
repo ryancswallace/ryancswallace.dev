5065f7f88842859a4f6bd1a23c8b5125f393f02e
Ryan Wallace <ryancwallace@pm.me>
2025-09-17 22:13:12 -0400

    fix: update TS config with site details

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,18 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://ryancswallace.dev/",
+  author: "Ryan Wallace",
+  profile: "https://ryancswallace.dev/",
+  desc: "Personal portfolio site built with Astro.",
+  title: "ryancswallace.dev",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
-  showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: true,
-    text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
-  },
+  showBackButton: true,
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  dir: "ltr",
+  lang: "en",
+  timezone: "US/Eastern",
 } as const;
