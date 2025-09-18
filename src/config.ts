91d94245b94006e0a92afea5f5a45a0d0f212ff0
Ryan Wallace <ryancwallace@pm.me>
2025-09-17 22:29:34 -0400

    fix: add editPost to SITE dictionary in TS config

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,11 @@ export const SITE = {
   title: "ryancswallace.dev",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
+  editPost: {
+    enabled: true,
+    text: "Edit page",
+    url: "https://github.com/ryancswallace/ryancswallace.dev/",
+  },
   postPerIndex: 4,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
